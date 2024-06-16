cf915010fcb71aa78a9333f81642ae365eb16384
ChromeEagle99 <chromeeagle99@gmail.com>
2024-06-16 09:56:26 +0800

    Added Image Slider, FoodKaki Post

---
+++ assets/js/featured-slider.js
@@ -0,0 +1,24 @@
+// assets/js/featured-slider.js
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      effect: 'coverflow',
+      coverflowEffect: {
+        rotate: 30,
+        slideShadows: false,
+      },
+    });
+  });
+  
